6010d92a17a7c1efadb33d9401b74919378935e4
Turlough Whelan <whelant44@gmail.com>
2024-05-11 11:23:57 +0100

    Making more changes r2

--- com/cmx/myTown.java
+++ com/cmx/myTown.java
@@ -3,5 +3,6 @@ package com.cmx;
 public class myTown {
     public static void main(String args[]) {
         System.out.println("hello carrick  \n");
+        System.out.println("I am a town \n");
     }
 }
